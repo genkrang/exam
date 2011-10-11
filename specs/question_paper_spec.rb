603c56d10178d6713bb50d8ccb3fac0eb9120826
Gen Krang <chukwukg@cs.man.ac.uk>
2011-10-11 14:47:52 +0100

    Exam project failure due to no question method defined

--- specs/question_paper_spec.rb
+++ specs/question_paper_spec.rb
@@ -8,4 +8,8 @@ require '../lib/question_paper.rb'
 describe QuestionPaper do
 
   subject {QuestionPaper.new}
+
+  it "should have an empty array of questions when QuestionPaper is created" do
+    subject.questions.should be_an_instance_of(Array)
+  end
 end
